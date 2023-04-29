e15b266b5429a7cd631be1ed32357d2e65199ab3
DaeHun <daehun1102@naver.com>
2023-04-29 20:11:53 +0900

    refact: add docs my_model.py
    #82

--- models/my_model.py
+++ models/my_model.py
@@ -8,6 +8,18 @@ import numpy as np
 from optimizers.loss import ArcMarginProduct
 
 class MaskResnet18(nn.Module):
+    """_summary_
+    
+    ResNet-18 기반의 마스크 착용 여부 분류 모델 클래스 (아래 다른 클래스들도 이와 동일하지만 
+    VIT,Swin Transformer,CaotNet 은 size를 224*224 또는 384*384 로 맞춰주어야 함)
+
+    Args:
+        num_classes (int): 분류할 클래스 수
+        x (torch.Tensor): 모델에 입력할 이미지 데이터. 크기는 (batch_size, channel=3, height, width)
+
+    Returns:
+        output (torch.Tensor): foward 결과 output. 크기는 (batch_size, num_classes)
+    """
     def __init__(self, num_classes):
         super().__init__()
         self.backbone = timm.create_model('resnet18',num_classes =num_classes,  pretrained=True)
@@ -117,7 +129,7 @@ class MaskSmallVIT(nn.Module):
         return x
     
 
-class MaskSwinSmallWindowVIT(nn.Module):
+class MaskSwinSmallWindow(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.backbone = timm.create_model('swin_small_patch4_window7_224',num_classes =num_classes, pretrained=True)
@@ -127,7 +139,7 @@ class MaskSwinSmallWindowVIT(nn.Module):
         return x
 
 
-class MaskSwinSmallVIT(nn.Module):
+class MaskSwinSmall(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.backbone = timm.create_model('swin_s3_small_224',num_classes =num_classes, pretrained=True)
@@ -137,7 +149,7 @@ class MaskSwinSmallVIT(nn.Module):
         return x
 
 
-class MaskSwinBaseVIT(nn.Module):
+class MaskSwinBase(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.backbone = timm.create_model('swin_s3_base_224',num_classes =num_classes, pretrained=True)
@@ -147,7 +159,7 @@ class MaskSwinBaseVIT(nn.Module):
         return x
 
 
-class MaskSwinBaseWindowVIT(nn.Module):
+class MaskSwinBaseWindow(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
         self.backbone = timm.create_model('swin_base_patch4_window7_224',num_classes =num_classes, pretrained=True)
@@ -181,11 +193,6 @@ class Coatnet(nn.Module):
     def __init__(self,num_classes):
         super().__init__()
         self.backbone = timm.create_model("hf_hub:timm/coatnet_rmlp_2_rw_384.sw_in12k_ft_in1k",num_classes=num_classes, pretrained=True)
-        #self.backbone = timm.create_model("hf_hub:timm/maxvit_small_tf_384.in1k", pretrained=True)
-        # self.backbone = timm.create_model("hf_hub:timm/convnextv2_base.fcmae_ft_in22k_in1k_384", pretrained=True)
-        # self.backbone = timm.create_model("swin_base_patch4_window12_384_in22k",num_classes=num_classes, pretrained=True)
-        # self.backbone = timm.create_model("hf_hub:timm/convnext_small.fb_in22k_ft_in1k_384", pretrained=True)
-        # self.classifier = nn.Linear(1000,num_classes,bias=True)
 
     def forward(self,x):
         x = self.backbone(x)
@@ -193,6 +200,17 @@ class Coatnet(nn.Module):
 
 
 class Canny(nn.Module):
+    """_summary_
+    
+    입력된 이미지(x)에 Canny edge detection을 적용한 결과를 추가적인 채널로 생성하여,
+    입력된 모델(backbone)을 통과시키는 모델
+
+    Args:
+        x (torch.Tensor): 크기가 (batch_size, channel=3+1, height, width)인 입력 이미지 텐서
+
+    Returns:
+        output (torch.Tensor): 모델의 순전파 연산 결과. 크기는 (batch_size, num_classes)
+    """
     def __init__(self,backbone):
         super().__init__()
         self.add_canny = nn.Conv2d(4,3,1)
@@ -215,6 +233,18 @@ class Canny(nn.Module):
     
     
 class ArcfaceModel(nn.Module):
+    """_summary_
+    
+    ArcFace 알고리즘을 사용하여 입력 이미지의 특징을 추출하고 분류를 수행하는 모델
+
+    Args:
+        backbone (nn.Module): 백본 신경망 모델
+        num_features (int): 입력 이미지의 특징 벡터의 크기
+        num_classes (int): 클래스 개수
+
+    Returns:
+        logits (torch.Tensor): ArcMarginProduct에 따라 margin 값을 계산한 output (batch_size, num_classes)
+    """
     def __init__(self,backbone,num_features, num_classes):
         super().__init__()
         self.backbone = backbone
@@ -227,6 +257,18 @@ class ArcfaceModel(nn.Module):
 
 
 class ArcfaceModelInfer(nn.Module):
+    """_summary_
+    
+    인퍼런스에 사용할 Arcface 모델
+
+    Args:
+        backbone (nn.Module): 백본 신경망 모델
+        num_features (int): 입력 이미지의 특징 벡터의 크기
+        num_classes (int): 클래스 개수
+
+    Returns:
+        logits (torch.Tensor): 분류 결과 output (batch_size, num_classes)
+    """
     def __init__(self,backbone,num_features, num_classes):
         super().__init__()
         self.backbone = backbone
